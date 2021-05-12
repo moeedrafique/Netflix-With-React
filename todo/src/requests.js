721d21e6563eeafe2b3b5b4b11f40b545b9fcf2d
Moeed Rafique <alimoeed15@gmail.com>
2021-05-12 20:51:53 +0500

    Initial Commit

---
+++ todo/src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "a6cf565ec8ec18337d1035e9dd0506b5";
+
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchKids: `/discover/movie?api_key=${API_KEY}&certification_country=US&certification.lte=G&sort_by=popularity.desc`,
+}
+
+export default requests;
